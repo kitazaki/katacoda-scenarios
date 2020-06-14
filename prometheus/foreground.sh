1529f983052d71705529c0cb98abd35c3817ea1f
kitazaki <kitazaki@gmail.com>
2020-06-15 00:11:43 +0900

    Add files via upload

--- prometheus/foreground.sh
+++ prometheus/foreground.sh
@@ -3,12 +3,12 @@ tar xvzf prometheus-2.19.0.linux-amd64.tar.gz
 cd prometheus-2.19.0.linux-amd64/
 mv prometheus.yml prometheus.yml.org
 wget https://raw.githubusercontent.com/kitazaki/katacoda-scenarios/master/prometheus/prometheus.yml
-./prometheus
+timeout 5 ./prometheus > /dev/null
 cd ../
 
 wget https://github.com/prometheus/node_exporter/releases/download/v1.0.0/node_exporter-1.0.0.linux-amd64.tar.gz
 tar xvzf node_exporter-1.0.0.linux-amd64.tar.gz
 cd node_exporter-1.0.0.linux-amd64/
-./node_exporter
+timeout 5 ./node_exporter > /dev/null
 cd ../
 
