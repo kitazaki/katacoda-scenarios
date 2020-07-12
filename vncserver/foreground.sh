3429dfb78032a9fa83db55ab7812318cb4c83bb4
kitazaki <kitazaki@gmail.com>
2020-07-12 10:22:37 +0900

    Add files via upload

--- vncserver/foreground.sh
+++ vncserver/foreground.sh
@@ -1,7 +1,9 @@
-sudo apt update
-sudo apt -y install chromium-browser
-
 sed s/5900/59000/ /usr/bin/vncserver > ./vncserver
 chmod 755 ./vncserver
 
+printf "password\npassword\n\n" | vnc4passwd
+nohup ./vncserver -geometry 1366x768
+
+sudo apt update
+sudo apt -y install chromium-browser
 
