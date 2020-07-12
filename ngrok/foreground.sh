c08a15c732240e74d83257ee69a8e113bcbbf231
kitazaki <kitazaki@gmail.com>
2020-07-12 19:55:13 +0900

    Add files via upload

---
+++ ngrok/foreground.sh
@@ -0,0 +1,5 @@
+wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip
+unzip *.zip
+./ngrok authtoken 6WmXHUKPtB1YRbMruN1ad_3eXyrP6sdYdJVZnNAZjnf
+./ngrok tcp 22
+
---
+++ ngrok/step1.md
@@ -0,0 +1,5 @@
+### 1. SOCKS proxy へアクセスします。
+```
+ssh -fND localhost:3129 -i id_rsa -p 10728 root@2.tcp.ngrok.io
+```{{copy}}
+
