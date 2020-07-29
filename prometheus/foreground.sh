6e262c8335b9ae34242df2daeac28b98720efb80
kitazaki <kitazaki@gmail.com>
2020-07-29 15:08:58 +0900

    Add files via upload

--- prometheus/foreground.sh
+++ prometheus/foreground.sh
@@ -4,6 +4,8 @@ wget https://github.com/prometheus/node_exporter/releases/download/v1.0.0/node_e
 tar xvzf node_exporter-1.0.0.linux-amd64.tar.gz
 wget https://github.com/prometheus/blackbox_exporter/releases/download/v0.17.0/blackbox_exporter-0.17.0.linux-amd64.tar.gz
 tar xvzf blackbox_exporter-0.17.0.linux-amd64.tar.gz
+wget https://github.com/prometheus/alertmanager/releases/download/v0.21.0/alertmanager-0.21.0.linux-amd64.tar.gz
+tar xvzf alertmanager-0.21.0.linux-amd64.tar.gz
 
 #wget https://bintray.com/artifact/download/nlamirault/oss/speedtest_exporter-0.3.0_linux_amd64
 #chmod 764 speedtest_exporter-0.3.0_linux_amd64
@@ -18,6 +20,12 @@ mv blackbox.yml blackbox.yml.org
 wget https://raw.githubusercontent.com/kitazaki/katacoda-scenarios/master/prometheus/blackbox.yml
 nohup ./blackbox_exporter --config.file="blackbox.yml" &
 cd ../
+
+#cd alertmanager-0.21.0.linux-amd64/
+#mv alertmanager.yml alertmanager.yml.org
+#wget https://raw.githubusercontent.com/kitazaki/katacoda-scenarios/master/prometheus/alertmanager.yml
+#nohup ./alertmanager &
+#cd ../
 
 cd prometheus-2.19.0.linux-amd64/
 mv prometheus.yml prometheus.yml.org
