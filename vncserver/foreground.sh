6fc2e3a88e1ee8fab706dd4c2d4f98469a0a518b
kitazaki <kitazaki@gmail.com>
2020-08-10 16:06:53 +0900

    Update foreground.sh

--- vncserver/foreground.sh
+++ vncserver/foreground.sh
@@ -1,12 +1,15 @@
 #sed s/5900/59000/ /usr/bin/vncserver > ./vncserver
 #chmod 755 ./vncserver
+#nohup ./vncserver -geometry 1366x768
+#./ngrok tcp 59001
+
+apt update
+apt -y install vnc4server xfce4 xfce4-goodies
 
 printf "password\npassword\n\n" | vnc4passwd
-#nohup ./vncserver -geometry 1366x768
 nohup vncserver -geometry 1366x768
 
 wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip
 unzip *.zip
 ./ngrok authtoken 6WmXHUKPtB1YRbMruN1ad_3eXyrP6sdYdJVZnNAZjnf
-#./ngrok tcp 59001
 ./ngrok tcp 5901
